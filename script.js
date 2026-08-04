49b90abea406c0ff77683baee32c4151ae2e221d
Rayane-G <rayane.walecki@escola.pr.gov.br>
2026-08-04 12:44:48 +0000

    aula 2

--- script.js
+++ script.js
@@ -44,8 +44,20 @@ const perguntas = [
 ];
 let atual = 0;
 let perguntaAtual;
+
 function mostraPergunta() {
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
 }
+function mostraAlternativas() {}
+
 mostraPergunta();
+
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
